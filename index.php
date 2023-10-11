98174ba24e4cb9e7f8d5595f8bc62baef88615d1
abject1 <lucaswurtz0@gmail.com>
2023-10-11 22:33:46 +0200

    mise en place de la base du projet

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./dist/css/mains.css"> <!-- link du css -->
+    <script defer src="./dist/js/mains.js"></script> <!-- link du js -->
+    <title>a_dev.bat | Accueil</title>
+</head>
+
+<body>
+    <h1>Bienvenue sur le site a_dev.bat</h1>
+</body>
+
+</html>
